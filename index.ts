46d97892a9a4e898e05b517a3bf7e5f765e35bea
Sergey Masiuk <mayerror@gmail.com>
2024-02-21 02:57:16 +0300

    feat: add meddleware

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { httpServer } from "./src/http_server";
-import { WebSocketServer } from "ws";
+import { RawData, WebSocketServer, WebSocket } from "ws";
 import * as http from "http";
 
 const HTTP_PORT = 8181;
@@ -10,13 +10,15 @@ wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data, isBinary) {
-    console.log(wss.clients.size);
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
-        console.log(data.toString());
       }
     });
+  });
+
+  ws.on("message", (data) => {
+    middleware(wss, data);
   });
 
   console.log("Connection established");
@@ -25,7 +27,7 @@ wss.on("connection", function connection(ws) {
 httpServer.listen(HTTP_PORT);
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 
-// const middleware = (
-//   wss: WebSocket.Server<typeof WebSocket, typeof http.IncomingMessage>,
-//   data
-// ) => {};
+const middleware = (wss: WebSocketServer, data: RawData) => {
+  console.log(wss.clients.size);
+  console.log(data.toString());
+};
