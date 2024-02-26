5d4932d45b9e752aa60644cdac73208cabcb25b1
Sergey Masiuk <mayerror@gmail.com>
2024-02-27 00:35:43 +0300

    fix: fix adding player to self room

--- src/modules/addUserToRoom.ts
+++ src/modules/addUserToRoom.ts
@@ -4,10 +4,11 @@ import type Room from "../types/room";
 function addUserToRoom(data: string, roomes: Room[], player: Player): Room | undefined {
   const { indexRoom } = JSON.parse(data);
   const room = roomes.find((room) => room.id === indexRoom);
-  if (room !== undefined && room.players.length === 1) {
+  if (room !== undefined && room.players.length === 1 && room.players[0].id !== player.id) {
     room.players.push(player);
+    return room;
   }
-  return room;
+  return undefined;
 }
 
 export default addUserToRoom;
